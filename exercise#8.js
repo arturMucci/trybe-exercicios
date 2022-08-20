28c143798954b901a72c8edcef71f97a2829aac3
arturMucci <artur.mucci@gmail.com>
2022-08-20 01:15:33 -0300

    lecture_4.2 - ex.8

---
+++ exercise#8.js
@@ -0,0 +1,10 @@
+/*
+8 - Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
+*/
+let array25 = [];
+
+for (let i = 0; i < 25; i += 1) {
+    array25[i] = i+1;
+}
+
+console.log(array25);
