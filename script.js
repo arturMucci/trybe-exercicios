701fc9f6bb1cdabdbdc847cae4c8cc7ddd435b81
arturMucci <artur.mucci@gmail.com>
2022-08-27 00:43:07 -0300

    lecture_5.1 - aula

---
+++ script.js
@@ -0,0 +1,5 @@
+const paragraph = document.getElementById("paragraph");
+  paragraph.style.color = "red";
+document.getElementById('page-title').innerText = 'Iron man';
+document.getElementById('paragraph').innerText = 'Estou achando esta aula muito interessante';
+document.getElementById('subtitle').style.backgroundColor = 'cyan';
