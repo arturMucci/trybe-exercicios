2f1badd3e795879949aab68ac0c97543e4321981
arturMucci <artur.mucci@gmail.com>
2022-09-06 13:46:52 -0300

    lecture_5.3 - ex.calendar2

--- calendar.js
+++ calendar.js
@@ -17,6 +17,7 @@ createDaysOfTheWeek();
 
 let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 const monthDays = document.getElementById('days');
+const btnContainer = document.getElementsByClassName('buttons-container')[0];
 
 function checkHoliday(day) {
   if (day.innerText == 4) {
@@ -49,4 +50,14 @@ function insertDays() {
   }
 }
 
-window.addEventListener('load', insertDays());
+function createHolidayBtn(string) {
+  const holidayBtn = document.createElement('button');  
+  holidayBtn.id = 'btn-holiday';
+  holidayBtn.innerText = string;
+  btnContainer.appendChild(holidayBtn);
+}
+
+window.addEventListener('load', () => {
+  insertDays();
+  createHolidayBtn('Feriados');
+});
