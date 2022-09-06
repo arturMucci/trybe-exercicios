73f7667824435df45b1904a048e451cde775b13e
arturMucci <artur.mucci@gmail.com>
2022-09-06 19:58:50 -0300

    lecture_5.3 - calendar.ex3

--- calendar.js
+++ calendar.js
@@ -18,35 +18,64 @@ createDaysOfTheWeek();
 let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 const monthDays = document.getElementById('days');
 const allDays = document.getElementsByClassName('day');
+const btnContainer = document.getElementsByClassName('buttons-container')[0];
+const holidays = document.getElementsByClassName('holiday');
+
+function changeClassName(teste) {
+  if (teste.innerText == 4) {
+    teste.className += ' friday';
+  }
+  if (teste.innerText == 11) {
+    teste.className += ' friday';
+  }
+  if (teste.innerText == 18) {
+    teste.className += ' friday';
+  }
+  if (teste.innerText == 24) {
+    teste.className += ' friday';
+  }
+  if (teste.innerText == 25) {
+    teste.className += ' holiday friday';
+  }
+  if (teste.innerText == 31) {
+    teste.className += ' holiday';
+  }
+  return teste;
+}
 
 function insertDays() {
   for (let index = 0; index < decemberDaysList.length; index += 1) {
     const day = document.createElement('li');
     day.className = 'day';
     day.innerText = decemberDaysList[index];
-    if (day.innerText == 4) {
-      day.className += ' friday';
-    }
-    if (day.innerText == 11) {
-      day.className += ' friday';
-    }
-    if (day.innerText == 18) {
-      day.className += ' friday';
-    }
-    if (day.innerText == 24) {
-      day.className += ' friday';
-    }
-    if (day.innerText == 25) {
-      day.className += ' holiday friday';
-    }
-    if (day.innerText == 31) {
-      day.className += ' holiday';
-    }
+    day.style.backgroundColor = 'rgb(238, 238, 238)';
+    day.style.borderRadius = '10%';
+    changeClassName(day);
     monthDays.appendChild(day);
+  }
+}
+
+function insertBtn() {
+  const btnHoliday = document.createElement('button');
+  btnHoliday.id = 'btn-holiday';
+  btnHoliday.innerText = 'Feriados';
+  btnHoliday.addEventListener('click', changeHolidayColors);
+  btnContainer.appendChild(btnHoliday);
+}
+
+function changeHolidayColors() {
+  if (holidays[0].style.backgroundColor === 'rgb(238, 238, 238)') {
+    for (let index = 0; index < holidays.length; index += 1) {
+      holidays[index].style.backgroundColor = 'rgb(200, 200, 200)';
+    }
+  } else  if (holidays[0].style.backgroundColor === 'rgb(200, 200, 200)') {
+    for (let index = 0; index < holidays.length; index += 1) {
+      holidays[index].style.backgroundColor = 'rgb(238, 238, 238)';
+    }
   }
 }
 
 window.addEventListener('load', () => {
   insertDays();
-  changeClassNames('Feriado');
+  insertBtn();
 });
