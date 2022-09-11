671b63087a703a5e852667fc8e26be9b81fa355d
arturMucci <artur.mucci@gmail.com>
2022-09-11 19:34:25 -0300

    recuperando o tempo perdido

--- program#1.js
+++ program#1.js
@@ -1,10 +1,11 @@
 /*
 1. Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
-Adição (a + b)
-Subtração (a - b)
-Multiplicação (a * b)
-Divisão (a / b)
-Módulo (a % b)
+  
+  Adição (a + b)
+  Subtração (a - b)
+  Multiplicação (a * b)
+  Divisão (a / b)
+  Módulo (a % b)
 */
 const a=5, b=10;
 let soma, sub, mult, div, mod;
