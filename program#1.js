4aa2acd3c145a37ef1a40131fa6bfedc06769966
arturMucci <artur.mucci@gmail.com>
2022-09-12 09:27:49 -0300

    deleted some archives

--- program#1.js
+++
@@ -1,6 +0,0 @@
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let menuServices = [];
-
-menuServices[0] = menu[1];
-
-console.log(menuServices);
--- program#2.js
+++
@@ -1,6 +0,0 @@
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let indexOfPortfolio = [];
-
-indexOfPortfolio[0] = menu[2];
-
-console.log(indexOfPortfolio);
