f3b29c154bb94b18b1579f289f5b060a25351ce9
arturMucci <artur.mucci@gmail.com>
2022-09-12 09:38:05 -0300

    conferindo os exercicios

--- exercise#2.5.js
+++ exercise#2.5.js
@@ -7,8 +7,8 @@ Valor esperado no retorno da função: 2.
 */
 let teste = [2, 3, 2, 5, 8, 2, 3];
 
-for (let i = 0; i < teste.length; i += 1) {
-  if () {
-
+for (let num1 in teste) {
+  for (let num2 in teste) {
+    
   }
 }
