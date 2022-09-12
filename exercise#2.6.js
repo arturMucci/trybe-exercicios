6cec3381808455ba628d26d8ccb3914245778ef6
arturMucci <artur.mucci@gmail.com>
2022-09-12 09:52:51 -0300

    lecture_4.4 - ex#2.6

---
+++ exercise#2.6.js
@@ -0,0 +1,18 @@
+/*
+6 - Crie uma função que receba um número natural (número inteiro não negativo) N e retorne o somatório de todos os números de 1 até N.
+Valor de teste: N = 5.
+Valor esperado no retorno da função: 1+2+3+4+5 = 15.
+*/
+
+let n = 7;
+
+function retornaSomatorio(n) {
+  let somatorio = 0;
+  for (let index = n; index > 0; index -= 1) {
+    somatorio += index;
+  }
+  return somatorio;
+}
+
+let resultado = retornaSomatorio(n);
+console.log(resultado);
