27ae76377dd4349fcde6511cf34cf460ccc24b25
arturMucci <artur.mucci@gmail.com>
2022-09-19 16:20:35 -0300

    fundamentals_7.1 - ex_pt2_3

--- exercise_pt2_2.js
+++ exercise_pt2_2.js
@@ -1,2 +1,3 @@
 longestWord = (string) => arrayString = string.split(' ').sort((a, b) => b.length - a.length)[0];
+
 console.log(longestWord('Antônio foi ao banheiro e não sabemos o que aconteceu'));
--- tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-
-// function longestWord(string) {
-//   const arrayString = string.split(' ');
-//   const newArray = arrayString.sort((a, b) => b.length - a.length)[0];
-//   console.log(newArray);
-// }
