4d0758585ed6da0aa525897f17aa3e43caf98161
arturMucci <artur.mucci@gmail.com>
2022-09-20 11:49:40 -0300

    fundamentals_7.3 - ex.2

--- script.test.js
+++ script.test.js
@@ -1,4 +1,4 @@
-const sum = require('./script');
+const {sum, myRemove} = require('./script');
 
 describe('Exercício 1 - The sum function: ', () => {
   test('1 - Testa se o retorno de sum(4, 5) é 9.', () => {
@@ -13,3 +13,15 @@ describe('Exercício 1 - The sum function: ', () => {
   test('3 - Testa se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5): ', () => {
     expect(() => sum(4, '5')).toThrow(Error)});
 });
+
+describe('Exercício 2 - removeMyArray: ', () => {
+  test('1 - Testa se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado: ', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+  });
+  test('1 - Testa se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado: ', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+  test('1 - Testa se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado: ', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
+})
