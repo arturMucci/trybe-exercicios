a42c304efea1e52cb8ba94001ebb453777279ff8
arturMucci <artur.mucci@gmail.com>
2022-09-23 18:07:07 -0300

    fundamentals_8.1 - pt2.1

--- pt1_ex2.js
+++ pt1_ex2.js
@@ -2,6 +2,19 @@
 // recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. 
 // O retorno da sua HOF deve ser uma string (Ex: “Tente novamente” ou “Parabéns você ganhou”).
 
-const randomNum = (max) => Math.round(Math.random() * max);
-const resultadoSorteio = (teste, randomNumber) => console.log(teste == randomNumber(5) ? 'Parabéns você ganhou' : 'Tente novamente');
-resultadoSorteio(1, randomNum);
+// const randomNum = (max) => Math.round(Math.random() * max);
+// const resultadoSorteio = (teste, randomNumber) => console.log(teste == randomNumber(5) ? 'Parabéns você ganhou' : 'Tente novamente');
+// resultadoSorteio(1, randomNum);
+
+const mage = {
+  healthPoints: 130,
+  intelligence: 45,
+  mana: 125,
+  damage: undefined,
+};
+
+const blabla = 30;
+
+console.log(Math['round'](Math['random']() * mage['mana']));
+
+
--- pt2_ex1.js
+++ pt2_ex1.js
@@ -18,10 +18,30 @@ const dragon = {
   damage: undefined,
 };
 
-const getRandomIntInclusive = (min, max) => {
+/* Crie uma função que retorna o dano do dragão.
+O dano será um número aleatório entre 15 (dano mínimo) e o valor do atributo strength (dano máximo). */
+
+// Crie uma função que retorna o dano causado pelo warrior.
+// O dano será um número aleatório entre o valor do atributo strength (dano mínimo) e o valor de strength * weaponDmg (dano máximo).
+
+// Crie uma função que retorna um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
+// O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) e o valor de intelligence * 2 (dano máximo).
+// A mana consumida por turno é 15. Além disto a função deve ter uma condicional, caso o mago tenha menos de 15 de mana o valor de dano recebe uma mensagem (Ex: “Não possui mana suficiente”) e a mana gasta é 0.
+
+const calcMana = (mana) => {
+  if (mana <= 15 ) {
+    return "Não possui mana suficiente";
+  }
+  return mana - 15;
+}
+
+const calcDmg = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-console.log
-
 const battleMembers = { mage, warrior, dragon };
+
+console.log(calcDmg(15, battleMembers.dragon.strength));
+console.log(calcDmg(battleMembers.warrior.strength, (battleMembers.warrior.strength * battleMembers.warrior.weaponDmg)));
+console.log(calcDmg(battleMembers.mage.intelligence, battleMembers.mage.intelligence * 2));
+console.log(calcMana(battleMembers.mage.mana));
