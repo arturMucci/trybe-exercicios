c582d3829174efb683f98d7e40d7d88af84ebfb4
arturMucci <artur.mucci@gmail.com>
2022-09-22 23:31:41 -0300

    fundamentals_8.1 - ex.3

--- ex3.js
+++ ex3.js
@@ -1,6 +1,3 @@
-const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
-const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
-
 // Crie uma HOF que receberá três parâmetros:
 // O primeiro será um array de respostas corretas (soluções);
 // O segundo será um array contendo as respostas fornecidas por uma pessoa estudante;
@@ -10,4 +7,27 @@ const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 // Uma resposta incorreta reduz a pontuação final em 0.5 ponto.
 // Ao final, a HOF deve retornar o total de pontos obtidos através das respostas fornecidas pela pessoa estudante. Utilize os seguintes arrays:
 
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
+const comparaArray = (array1, array2) => {
+  let gabarito = 0;
+  for(let index = 0; index < array1.length; index += 1) {
+    if (array2[index] === array1[index]) {
+      gabarito += 1;
+    }
+    if (array2[index] === 'N.A') {
+      gabarito += 1;
+    }
+    if (array2[index] !== array1[index]) {
+      gabarito -= 0.5;
+    }
+  }
+  return gabarito;
+}
+
+const corrigeProva = (rightA, studentA, func) => {
+  return console.log(func(rightA, studentA));
+}
+
+corrigeProva(RIGHT_ANSWERS, STUDENT_ANSWERS, comparaArray);
