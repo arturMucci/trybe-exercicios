0b74745fae28a7d21c14962050518c27a142fd1a
arturMucci <artur.mucci@gmail.com>
2022-09-25 10:48:28 -0300

    fundamentals_8.1 - Ex.Bonus

--- pt2_ex1.js
+++ pt2_ex1.js
@@ -18,30 +18,47 @@ const dragon = {
   damage: undefined,
 };
 
-/* Crie uma função que retorna o dano do dragão.
-O dano será um número aleatório entre 15 (dano mínimo) e o valor do atributo strength (dano máximo). */
+// Crie uma função que retorna o dano do dragão.
+// O dano será um número aleatório entre 15 (dano mínimo) e o valor do atributo strength (dano máximo).
+const charDmg = (min, max) => {
+  return Math.round(Math.random(min) * max);
+};
+const dragonDmg = charDmg(15, dragon.strength);
 
 // Crie uma função que retorna o dano causado pelo warrior.
 // O dano será um número aleatório entre o valor do atributo strength (dano mínimo) e o valor de strength * weaponDmg (dano máximo).
+const warriorDmg = charDmg(warrior.strength, warrior.strength * warrior.weaponDmg);
 
 // Crie uma função que retorna um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
 // O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) e o valor de intelligence * 2 (dano máximo).
 // A mana consumida por turno é 15. Além disto a função deve ter uma condicional, caso o mago tenha menos de 15 de mana o valor de dano recebe uma mensagem (Ex: “Não possui mana suficiente”) e a mana gasta é 0.
-
-const calcMana = (mana) => {
-  if (mana <= 15 ) {
-    return "Não possui mana suficiente";
+const mageDmg = () => {
+  mage.mana -= 15;
+  if (mage.mana >= 15) {
+    const retorno = {
+      'dano': charDmg(mage.intelligence, mage.intelligence * 2),
+      'spendedMana': 15
+    }
+    return retorno;
+  } else {
+    return 'Not enough mana!';
   }
-  return mana - 15;
 }
 
-const calcDmg = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+const warriorShift = (dmg, target) => {
+  target.healthPoints -= dmg;
+}
+
+const mageshift = (dmg, target) => {
+
 }
 
 const battleMembers = { mage, warrior, dragon };
+const gameActions = {
+  'warriorShift': warriorShift(warriorDmg, dragon),
+  'mageshift': mageshift(mageDmg, dragon)
+};
 
-console.log(calcDmg(15, battleMembers.dragon.strength));
-console.log(calcDmg(battleMembers.warrior.strength, (battleMembers.warrior.strength * battleMembers.warrior.weaponDmg)));
-console.log(calcDmg(battleMembers.mage.intelligence, battleMembers.mage.intelligence * 2));
-console.log(calcMana(battleMembers.mage.mana));
+console.log(gameActions.warriorShift);
+console.log(dragon);
+
